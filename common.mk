40170a2f7e964561d281958ba5034374839ff5b6
someone5678 <someone5678@users.noreply.github.com>
2024-06-08 22:59:05 +0900

    qcs8550-common: Build QCOM USB gadget HAL
    * Newer SoC's like kalama use gadget HAL
    * Import usb_compositions.conf from stock for later customization
    
    Change-Id: Ib3383bc1173db22c539b2073d72419c42fcf92cc

--- common.mk
+++ common.mk
@@ -158,10 +158,14 @@ PRODUCT_PACKAGES += \
 
 # USB
 PRODUCT_PACKAGES += \
-    android.hardware.usb-service.qti
+    android.hardware.usb-service.qti \
+    android.hardware.usb.gadget-service.qti
 
 PRODUCT_SOONG_NAMESPACES += \
     vendor/qcom/opensource/usb/etc
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/usb/usb_compositions.conf:$(TARGET_COPY_OUT_ODM)/etc/usb_compositions.conf
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.usb.accessory.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.accessory.xml \
