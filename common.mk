06e5a1804f7d4cc992c4c5ef2aa38a942066b8a3
AnierinB <anierin@evolution-x.org>
2025-01-03 14:06:41 +0100

    qcs8550-common: Add kernel flags
    Change-Id: I8e4c059c6a9d5774864bc745b8696f90fb86b5ec
    Signed-off-by: AnierinB <anierin@evolution-x.org>

--- common.mk
+++ common.mk
@@ -7,6 +7,10 @@
 BOARD_SHIPPING_API_LEVEL := 33
 PRODUCT_SHIPPING_API_LEVEL := $(BOARD_SHIPPING_API_LEVEL)
 
+# Kernel
+PRODUCT_COPY_FILES += \
+    kernel/ayn/qcs8550/modules.systemdlkm_blocklist.msm.kalama:$(TARGET_COPY_OUT_VENDOR_DLKM)/lib/modules/system_dlkm.modules.blocklist
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
