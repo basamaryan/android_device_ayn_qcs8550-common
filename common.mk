34b55b5f2ad4f93c878ce0966b99ccd619e9491a
LuK1337 <priv.luk@gmail.com>
2025-01-17 10:26:03 +0100

    qcs8550-common: Build ipacm
    Change-Id: I2239baa628e2d0b58664e3340a05cd065ad109e3

--- common.mk
+++ common.mk
@@ -82,6 +82,12 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.health-service.qti
 
+# IPACM
+PRODUCT_PACKAGES += \
+    ipacm \
+    IPACM_cfg.xml \
+    IPACM_Filter_cfg.xml
+
 # Init
 PRODUCT_PACKAGES += \
     fstab.default \
