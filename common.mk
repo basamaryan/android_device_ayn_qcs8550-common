8e5025f71afb224a0fe517e81b7fcee10a8f2ccd
LuK1337 <priv.luk@gmail.com>
2025-01-17 10:25:53 +0100

    qcs8550-common: Build QTI health HAL
    Change-Id: I00069d339a74237d63ac8a2233804cba72685dc6

--- common.mk
+++ common.mk
@@ -78,6 +78,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/tablet_core_hardware.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/tablet_core_hardware.xml
 
+# Health
+PRODUCT_PACKAGES += \
+    android.hardware.health-service.qti
+
 # Init
 PRODUCT_PACKAGES += \
     fstab.default \
