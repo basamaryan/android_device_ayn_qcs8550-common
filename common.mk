2352b03e473fa4db327d048ad99c9b523fb5040f
LuK1337 <priv.luk@gmail.com>
2025-01-18 20:12:43 +0100

    qcs8550-common: overlay: Configure lineage-sdk
    Change-Id: I343b41c376be51d17d46e9f8c25d6a5bed27121f

--- common.mk
+++ common.mk
@@ -188,6 +188,7 @@ PRODUCT_ENFORCE_RRO_TARGETS := *
 PRODUCT_PACKAGES += \
     FrameworksResCommon \
     FrameworksResTarget \
+    LineageSDKOverlayCommon \
     NcmTetheringOverlay \
     SettingsResCommon \
     SystemUIResCommon \
