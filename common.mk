049f2a3d131d676eb0ee4883d89fc417ea116552
LuK1337 <priv.luk@gmail.com>
2025-01-27 00:48:16 +0100

    qcs8550-common: Build QTI thermal HAL
    Change-Id: Iad709075d2390ff15cfd886f4555acdcba2de65f

--- common.mk
+++ common.mk
@@ -150,6 +150,10 @@ PRODUCT_SOONG_NAMESPACES += \
 # Storage
 $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 
+# Thermal
+PRODUCT_PACKAGES += \
+    android.hardware.thermal-service.qti
+
 # Update engine
 PRODUCT_PACKAGES += \
     update_engine \
