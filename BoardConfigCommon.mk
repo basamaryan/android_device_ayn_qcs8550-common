0130a7ad922c6a3aba75aa65447f9437c2444402
AnierinB <anierin@evolution-x.org>
2026-01-20 02:26:51 -0800

    qcs8550-common: Initial common tree skeleton
    Signed-off-by: AnierinB <anierin@evolution-x.org>

---
+++ BoardConfigCommon.mk
@@ -0,0 +1,9 @@
+#
+# SPDX-FileCopyrightText: The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+#
+
+COMMON_PATH := device/ayn/qcs8550-common
+
+# Include the proprietary files BoardConfig.
+include vendor/ayn/qcs8550-common/BoardConfigVendor.mk
---
+++ common.mk
@@ -0,0 +1,15 @@
+#
+# SPDX-FileCopyrightText: The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Shipping API
+BOARD_SHIPPING_API_LEVEL := 33
+PRODUCT_SHIPPING_API_LEVEL := $(BOARD_SHIPPING_API_LEVEL)
+
+# Soong namespaces
+PRODUCT_SOONG_NAMESPACES += \
+    $(LOCAL_PATH)
+
+# Inherit from the proprietary files makefile.
+$(call inherit-product, vendor/ayn/qcs8550-common/qcs8550-common-vendor.mk)
